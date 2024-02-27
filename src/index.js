d030b9576b5962f154db5b32e318423f22c888ec
Kristen <kgrace61@gmail.com>
2024-02-26 19:12:28 -0600

    completes lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form")
+
+    inputForm.addEventListener("submit", (event)=>{
+        event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p");
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+    });
+});
 }
 
+
 document.addEventListener('DOMContentLoaded', init);
+
+//add event listeners to capture form data
+//override a forms default behavior (e.preventDefault())
+//fetch data based on what the user types into the form
+//display the data on the page (is this appending to the page? or rendering)
